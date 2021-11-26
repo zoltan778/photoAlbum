3db0eb50e3a2018227091348789d3b0d029f69b7
mzoltan778 <zoltanmeszaros977@gmail.com>
2021-11-26 20:43:42 +0100

    deploy

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+
+app.use(cors());
+app.use(fileUpload());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", express.static(__dirname + "/public"));
+
+app.use("/api/user", require("./routes/user.route"));
+app.use("/api/photo", require("./routes/photo.route"));
+app.use("/api/album", require("./routes/album.route"));
+app.get("/api/test", (req, res) => {
+  return res.json({ msg: "testing" });
+});
+app.get('*',function(req,res){
+    res.sendFile(__dirname + "/public/index.html")
+});
+app.use(require("./middleware/errorHandler"));
+
+module.exports = app;
